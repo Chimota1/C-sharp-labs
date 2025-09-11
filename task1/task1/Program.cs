7620216f979d36e9eb8f871fa436038269ecd504
Anatoliy <avkindrat2006@gmail.com>
2025-09-11 16:50:43 +0300

    Do logic for Isinclude, wich matched in rectangle

--- task1/task1/Program.cs
+++ task1/task1/Program.cs
@@ -2,10 +2,23 @@
 
 using System;
 
+
 class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        const int R = 5;
+        const int R = 15;
+        Console.WriteLine("Enter the coordinates of the point x: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the coordinates of the point y: ");
+        int y = int.Parse(Console.ReadLine());
+        Isinclude(x,y,R);
+    }
+    public static void Isinclude(int x, int y, int R)
+    {
+        if ((x > 0 && x < (2*R)) && (y > -R && y < 0))
+        {
+            Console.WriteLine("The point match in rectangle");
+        }
     }
 }
--- task1/task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ task1/task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca0ab16685ab4978728c6699281ed13c0b10679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f34c021ecadc4d3c178396b2db639062fce263")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
